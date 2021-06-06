b9ef3ee177b5c440f20da4d865445c70387d6c69
masud-ahmed-alt <ma7332125@gmail.com>
2021-06-07 02:15:27 +0530

    add user validation method

--- app/src/main/java/www/fanio/com/LoginActivity.java
+++ app/src/main/java/www/fanio/com/LoginActivity.java
@@ -5,20 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageView login;
     private TextView signUp;
+    private EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        getSupportActionBar().hide();
+
         login = findViewById(R.id.login);
         signUp = findViewById(R.id.switch_signup);
+        email = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,10 +41,27 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                validateUser();
             }
         });
+    }
+
+    private void validateUser() {
+        String username, user_password;
+        username = email.getText().toString();
+        user_password = password.getText().toString();
+
+        if (username.isEmpty()){
+            email.setError("Username Empty");
+            email.requestFocus();
+        }else if (user_password.isEmpty()){
+            password.setError("Password Empty");
+            password.requestFocus();
+        }else{
+            Intent intent= new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }
--- app/src/main/java/www/fanio/com/SignUpActivity.java
+++ app/src/main/java/www/fanio/com/SignUpActivity.java
@@ -5,21 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
     private ImageView signUp;
     private TextView login;
+    private EditText signup_email, signup_mobile, signup_password, signup_confirmPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
 
-        signUp = findViewById(R.id.signup);
+        signUp = findViewById(R.id.btn_signup);
         login = findViewById(R.id.switch_login);
+        signup_email = findViewById(R.id.signup_email);
+        signup_mobile = findViewById(R.id.signup_mobile);
+        signup_password = findViewById(R.id.signup_password);
+        signup_confirmPass = findViewById(R.id.signup_confirmPass);
 
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateDate();
+            }
+        });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +43,39 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void validateDate() {
+        String email = signup_email.getText().toString();
+        String mobile = signup_mobile.getText().toString();
+        String password = signup_password.getText().toString();
+        String confirmPass = signup_confirmPass.getText().toString();
+
+        if (email.isEmpty()){
+            signup_email.setError("Field can't be empty");
+            signup_email.requestFocus();
+        }else if (mobile.isEmpty()){
+            signup_mobile.setError("Field can't be empty");
+            signup_mobile.requestFocus();
+        }else if (mobile.length()>10){
+            signup_mobile.setError("Invalid Mobile Number");
+            signup_mobile.requestFocus();
+        }
+
+        else if (password.isEmpty()){
+            signup_password.setError("Field can't be empty");
+            signup_password.requestFocus();
+        }else if (confirmPass.isEmpty()){
+            signup_confirmPass.setError("Field can't be empty");
+            signup_confirmPass.requestFocus();
+        }else if (!confirmPass.equals(password)){
+            signup_confirmPass.setError("Password not matched");
+            signup_confirmPass.requestFocus();
+        }else {
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            Toast.makeText(this, "Account created successfully!", Toast.LENGTH_LONG).show();
+            startActivity(intent);
+            finish();
+        }
     }
 }
