544a5a412ed905964054e2ed235cca191fe25fbd
masud-ahmed-alt <ma7332125@gmail.com>
2021-06-07 10:50:41 +0530

    Design Modified

--- app/src/main/java/www/fanio/com/SignUpActivity.java
+++ app/src/main/java/www/fanio/com/SignUpActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
-    private ImageView signUp;
+    private Button signUp;
     private TextView login;
     private EditText signup_email, signup_mobile, signup_password, signup_confirmPass;
 
